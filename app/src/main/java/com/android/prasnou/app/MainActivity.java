bbdaa48ab2641f1768bb46d4dbd7d5df130b66c7
SeaView <BlackPine09@gmail.com>
2016-09-06 20:10:05 -0700

    some changes

--- app/src/main/java/com/android/prasnou/app/MainActivity.java
+++ app/src/main/java/com/android/prasnou/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.prasnou.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
     }
 
     @Override
